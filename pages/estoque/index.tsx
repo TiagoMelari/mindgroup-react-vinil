cdf83abe05bdf1262422b718af6aa0a3d8d16f5b
Tiago THermes <101218962+TMHermes@users.noreply.github.com>
2024-04-23 23:18:03 -0300

    voltando a lógica de recebimento de produtos e eliminando o array fictício

--- pages/estoque/index.tsx
+++ pages/estoque/index.tsx
@@ -1,41 +1,10 @@
 import Head from 'next/head';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Produto from '../../types/Produto';
 import ProdutoModal from '../../modals/ProdutoModal';
 
-const produtosFicticios = [
-    {
-        produto_id: 1,
-        nome: 'Vinil 1',
-        descricao: 'Descrição do Vinil 1',
-        valor: 29.99,
-        qtd_estoque: 10
-    },
-    {
-        produto_id: 2,
-        nome: 'Vinil 2',
-        descricao: 'Descrição do Vinil 2',
-        valor: 39.99,
-        qtd_estoque: 5
-    },
-    {
-        produto_id: 3,
-        nome: 'Vinil 3',
-        descricao: 'Descrição do Vinil 3',
-        valor: 19.99,
-        qtd_estoque: 15
-    },
-    {
-        produto_id: 4,
-        nome: 'Vinil 4',
-        descricao: 'Descrição do Vinil 4',
-        valor: 49.99,
-        qtd_estoque: 8
-    }
-];
-
 export default function Estoque() {
-    const [produtos] = useState<Produto[]>(produtosFicticios);
+    const [produtos, setProdutos] = useState<Produto[]>([]);
     const [produtoSelecionado, setProdutoSelecionado] = useState<Produto | null>(null);
     const [novoProduto, setNovoProduto] = useState<Partial<Produto>>({
         nome: '',
@@ -43,6 +12,20 @@ export default function Estoque() {
         valor: 0,
         qtd_estoque: 0
     });
+
+    const buscaProdutos = async () => {
+        try {
+            const response = await fetch('/pags/api/estoque');
+            const data = await response.json();
+            setProdutos(data);
+        } catch (error) {
+            console.error('Erro ao buscar produtos:', error);
+        }
+    }
+
+    useEffect(() => {
+        buscaProdutos();
+    }, []);
 
     const handleAbreModal = (produto: Produto) => {
         setProdutoSelecionado(produto);
@@ -54,6 +37,8 @@ export default function Estoque() {
 
     const handleAdicionarProduto = async () => {
         try {
+
+
             const response = await fetch('/api/estoque', {
                 method: 'POST',
                 headers: {
@@ -87,7 +72,7 @@ export default function Estoque() {
             <main className="main">
                 <h1 className="title">Estoque da Vinil Store</h1>
 
-                <button id='button-estq' onClick={handleAdicionarProduto}>Adicionar Produto</button> 
+                <button id='button-estq' className='button' onClick={handleAdicionarProduto}>Adicionar Produto</button> 
 
                 <div className="grid">
                     {produtos.map((produto, index) => (
